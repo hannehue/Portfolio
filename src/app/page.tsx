9b315a76fdec7e173515a731078065e97e8c933f
hannehue <Hannehue@gmail.com>
2025-05-12 00:59:56 +0200

    Slide in, slide out feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import styles from "./page.module.css";
 import HomeProjectItem from "@/components/homeProjectItem";
 
@@ -15,14 +17,17 @@ export default function Home() {
                 <HomeProjectItem title={"DISCO 2 STORAGE SYSTEM"}
                                  description={"Database, data-ingestion and data-access system for the Danish Cubesat Program (DISCO)" +
                                      "satellite DISCO 2, enabling seasoned scientists and new, to query observations in a" +
-                                     "meaningful and easy to understand way to support their research"}/>
+                                     "meaningful and easy to understand way to support their research"}
+                                 bulletPoints={["Requirements-engineering", "Data modelling", "Go-API"]}/>
                 <HomeProjectItem title={"WORKOUT TRACKER"}
                                  description={"Personal project to track progress of my weightlifting. A project also mainly done to learn " +
-                                     "Spring and Spring boot"}/>
+                                     "Spring and Spring boot"}
+                                 bulletPoints={["Spring/Spring Boot"]}/>
                 <HomeProjectItem title={"DEV-OPS & CI/CD"}
                                  description={"University course in devops principles and technologies, utilizing tools like Github Actions, " +
-                                     "Docker, Kubernetes to build a full fledged pipeline with testing, staging and production" +
-                                     "deployments. Theory to understand the devops mindset, and how to promote the culture"}/>
+                                     "Docker, Kubernetes to build a full fledged pipeline with testing, staging and production " +
+                                     "deployments. Theory to understand the devops mindset, and how to promote the culture"}
+                                 bulletPoints={["Github Actions", "Docker", "Kubernetes"]}/>
             </main>
             <footer className={styles.footer}>
             </footer>
--- src/components/homeProjectItem.tsx
+++ src/components/homeProjectItem.tsx
@@ -1,12 +1,37 @@
 import styles from "@/app/page.module.css";
+import {useEffect, useLayoutEffect, useRef, useState} from "react";
 
-export default function HomeProjectItem({title, description}: {title: string, description: string}) {
+export default function HomeProjectItem({title, description, bulletPoints}: {title: string, description: string, bulletPoints: string[]}) {
+
+    const [showAlt, setShowAlt] = useState<boolean>(false)
+    const [height, setHeight] = useState(0)
+    const ref = useRef(null)
+
+    const points = bulletPoints.map(point =>
+        <li key={crypto.randomUUID()}>{point}</li>);
+
+    useEffect(() => console.log(ref), [])
+
+    useLayoutEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-expect-error
+        setHeight(ref.current!.clientHeight)
+
+    }, []);
 
     return (
-        <div className={styles.projects}>
-            <div className={styles.pItem}>
+        <div className={styles.projects} onMouseEnter={() => setShowAlt(true)} onMouseLeave={() => setShowAlt(false)}>
+            <div className={`${styles.pItem}`} >
                 <h3>{title}</h3>
-                <p>{description}</p>
+                <div  style={{maxHeight: (height)}} className={styles.pText}>
+                    <p ref={ref} className={`${showAlt ? styles.slideUp : styles.slideDown}`}>{description}</p>
+                    <div style={{height: height}} className={`${showAlt ? styles.slideUp : styles.slideDown}`}>
+                        <a href={"/"} >
+                            <ul>{points}</ul>
+                            Go to project
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     )
